461948f3130130b2b031980decc304eb5a73365d
Rukiye Demirci <71218414+DRukiye@users.noreply.github.com>
2020-10-17 11:26:37 +0300

    Update ödev3.3.c

--- İlk Hw'ler/ödev3.3.c
+++ İlk Hw'ler/ödev3.3.c
@@ -3,48 +3,46 @@
 
 int main()
 {
-	    int h;
+	int h;
 	    
-	    printf("yükseklik: ");
-	    scanf("%d",&h); 
+	printf("yükseklik: ");
+	scanf("%d",&h); 
         
-		 int basamak=1;
-         int bosluk = h-1;
+        int basamak=1;
+        int bosluk = h-1;
           
-            int i,j;
-            for(i=0; i<h;i++)
-            {
-               for(j=0;j<bosluk;j++)
-               {
-                  printf(" ");
-               }
-               for(j=0;j<basamak;j++)
-               { 
-                  printf("#");
-               }
-                basamak +=2;
-                bosluk--;
-                printf("\n");             
-            }  
-			basamak -=4;
-			bosluk=1;  
+        int i,j;
+        for(i=0; i<h;i++)
+        {
+             for(j=0;j<bosluk;j++)
+             {
+                printf(" ");
+             }
+             for(j=0;j<basamak;j++)
+             { 
+                printf("#");
+             }
+             basamak +=2;
+             bosluk--;
+             printf("\n");             
+        }  
+	basamak -=4;
+	bosluk=1;  
    			
-   			for(i=0; i<h;i++)
-            {
-                for(j=0;j<bosluk;j++)
-                {
-                    printf(" ");
-                }
-                for(j=0;j<basamak;j++)
-                { 
-                   printf("#");
-                }
-                printf("\n");
-                bosluk++;
-                basamak -=2;               
-            }  
-   			
-   
-   
+   	for(i=0; i<h;i++)
+        {
+             for(j=0;j<bosluk;j++)
+             {
+                 printf(" ");
+             }
+             for(j=0;j<basamak;j++)
+             { 
+                 printf("#");
+             }
+             printf("\n");
+             bosluk++;
+             basamak -=2;               
+         }  
+
    return 0;
 }
